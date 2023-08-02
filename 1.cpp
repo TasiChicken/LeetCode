84dc534ad34205034a9c5e6b020c6e2900844b87
TasiChicken <ien940714@gmail.com>
2023-08-02 23:41:48 +0800

    init

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+
+int main()
+{
+    Solution solution;
+    
+    string input;
+    vector<int> nums;
+    int target;
+
+    cout << "nums = ";
+    cin >> input;
+    cout << "target = ";
+    cin >> target;
+
+    return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+
+};
+
+int main()
+{
+    Solution solution;
+
+    return 0;
+}
