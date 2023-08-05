5d667e99d531b1e665b52649775b62ef63e3064d
TasiChicken <ien940714@gmail.com>
2023-08-05 21:20:10 +0800

    finish 5

--- 5.cpp
+++ 5.cpp
@@ -4,12 +4,45 @@ using namespace std;
 
 class Solution {
 public:
+    string longestPalindrome(string s) {
+        int len = s.length();
 
+        int ansIndex = 0;
+        int ansLen = 1;
+        for(int i = 0; i < len - 1; i++) {
+            int delta = 1;
+            while(i - delta >= 0 && i + delta < len && s[i - delta] == s[i + delta]) 
+                delta++;
+            
+            if(--delta * 2 + 1 > ansLen) {
+                ansIndex = i - delta;
+                ansLen = delta * 2 + 1;
+            }
+
+            delta = 0;
+            while(i - delta >= 0 && i + delta + 1 < len && s[i - delta] == s[i + delta + 1]) 
+                delta++;
+            
+            if(delta * 2 > ansLen) {
+                ansIndex = i - delta + 1;
+                ansLen = delta * 2;
+            }
+        }
+
+        return s.substr(ansIndex, ansLen);
+    }
 };
 
 int main()
 {
     Solution solution;
+
+    while(true){
+        string s;
+        cout << "s = ";
+        cin >> s;
+        cout << solution.longestPalindrome(s) << endl;
+    }
 
     return 0;
 }
