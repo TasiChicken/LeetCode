b49bd35cb54c02c6d06da7d359da82990fd20ce4
TasiChicken <ien940714@gmail.com>
2023-08-12 14:39:07 +0800

    finish 24

---
+++ 24.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        auto next = head->next;
+        head->next = swapPairs(head->next->next);
+        next->next = head;
+        return next; 
+    }
+};
+
+ListNode* parse_input(string input) {
+    ListNode* first;
+    input.erase(input.length() - 1, 1);
+    input.erase(0, 1);
+    
+    if(input.length() == 0) return first;
+
+    int index = 0;
+    ListNode* last;
+    for(int i = 0; i <= input.length(); i++) 
+        if(input[i] == ',' || i == input.length()) {
+            int value = stoi(input.substr(index, i - index));
+
+            if(index == 0) first = last = new ListNode(value);
+            else {
+                last->next = new ListNode(value);
+                last = last->next;
+            }
+            index = i + 1;
+        }
+
+    return first;
+}
+
+string print_output(ListNode* output) {
+    string output_string = "[";
+    for(; output != NULL; output = output->next) {
+        output_string += to_string(output->val);
+        if(output->next != nullptr)
+            output_string += ",";
+    }
+    output_string += "]";
+
+    return output_string;
+}
+
+int main()
+{
+    Solution solution;
+
+    while(true) {
+        string input;
+        ListNode *head;
+        int n;
+
+        cout << "head = ";
+        cin >> input;
+        head = parse_input(input);
+
+        cout << print_output(solution.swapPairs(head)) << endl;
+    }
+    
+    return 0;
+}
