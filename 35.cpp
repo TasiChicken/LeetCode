f21f9330b84a49d341d59a85619c01c6474064ca
TasiChicken <ien940714@gmail.com>
2023-08-17 11:27:01 +0800

    finish 34

---
+++ 35.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+
+};
+
+int main()
+{
+    Solution solution;
+
+    while(true) {
+        //code here
+    }
+    
+    return 0;
+}
