b11adb21f4e4290ec670476369ce8f52babfa761
TasiChicken <ien940714@gmail.com>
2024-02-14 23:33:12 +0800

    update 319

--- cpps/1000/400/20/319.cpp
+++ cpps/1000/400/20/319.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 class Solution {
 public:
     int bulbSwitch(int n) {
-        int i = 0;
-        while(i * i <= n) i++;
-        return i - 1;
+        return sqrt(n);
     }   
 };
 
