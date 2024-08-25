57279d536735e9012eed2baf5eee6aa2a3d88ba5
TsaiChicken <ien940714@gmail.com>
2024-08-25 14:38:55 +0800

    finish 437

---
+++ cpps/1000/500/40/437.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* parse(string input) {
+    TreeNode* head = nullptr;
+    input.erase(input.length() - 1, 1);
+    input.erase(0, 1);
+    
+    if(input.length() == 0) return head;
+
+    int index = 0;
+    bool left = true;
+    queue<TreeNode*> q;
+    for(int i = 0; i <= input.length(); i++) 
+        if(input[i] == ',' || i == input.length()) {
+            auto sub = input.substr(index, i - index);
+
+            TreeNode* node = nullptr;
+            if(sub != "null") node = new TreeNode(stoi(input.substr(index, i - index)));
+            index = i + 1;
+
+            if(node) q.push(node);
+            if(!head){
+                head = node;
+                continue;
+            } 
+
+            if(left) q.front()->left = node;
+            else q.front()->right = node, q.pop();
+            left = !left;
+        }
+
+    return head;
+}
+
+void print(TreeNode* output) {
+    cout << '[';
+    queue<TreeNode*> q;
+    int count = output != nullptr;
+    for(q.push(output); count && !q.empty(); q.pop()) {
+        if(q.front()){
+            cout << q.front()->val;
+            q.push(q.front()->left);
+            q.push(q.front()->right);
+
+            count = count - 1 + (q.front()->left != nullptr) + (q.front()->right != nullptr);
+        }
+        else cout << "null";
+
+        if(count) cout << ',';
+    }
+    cout << ']' << endl;
+}
+
+class Solution {
+    int pathSum(TreeNode* root, int targetSum, long long prev) {
+        if(!root) return 0;
+        return (prev + root->val == targetSum) +
+            pathSum(root->left, targetSum, prev + root->val) + 
+            pathSum(root->right, targetSum, prev + root->val);
+    }
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        if(!root) return 0;
+        return pathSum(root, targetSum, 0) + 
+            pathSum(root->left, targetSum) + 
+            pathSum(root->right, targetSum);
+    }
+};
+
+int main() {
+    while(true) {
+        Solution solution;
+        string s;
+        cout << "root = ";
+        cin >> s;
+        auto root = parse(s);
+        int targetSum;
+        cout << "targetSum = ";
+        cin >> targetSum;
+
+        cout << solution.pathSum(root, targetSum) << endl;
+    }
+    
+    return 0;
+}
