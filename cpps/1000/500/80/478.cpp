428a14f61cafcfd07bee44b37ef4bb0259607b6a
TsaiChicken <ien940714@gmail.com>
2024-12-24 03:11:41 +0800

    finish 478

---
+++ cpps/1000/500/80/478.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    double radius, x_center, y_center;
+public:
+    Solution(double radius, double x_center, double y_center) {
+        this->radius = radius;
+        this->x_center = x_center;
+        this->y_center = y_center;
+    }
+    
+    vector<double> randPoint() {
+        double radius = (double)rand() / RAND_MAX * this->radius;
+        double angle = (double)rand() / RAND_MAX * 2 * M_PI;
+        
+        vector<double> ans(2);
+        ans[0] = radius * cos(angle);
+        ans[1] = radius * sin(angle);
+        return ans;
+    }
+};
