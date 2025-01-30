b5671ca10ea1118c946707a90e3f0051e24b2164
TasiChicken <ien940714@gmail.com>
2025-01-30 15:47:40 +0800

    finish 501

---
+++ cpps/1000/600/10/501.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* parse(string input) {
+    TreeNode* head = nullptr;
+    input.erase(input.length() - 1, 1);
+    input.erase(0, 1);
+    
+    if(input.length() == 0) return head;
+
+    int index = 0;
+    bool left = true;
+    queue<TreeNode*> q;
+    for(int i = 0; i <= input.length(); i++) 
+        if(input[i] == ',' || i == input.length()) {
+            auto sub = input.substr(index, i - index);
+
+            TreeNode* node = nullptr;
+            if(sub != "null") node = new TreeNode(stoi(input.substr(index, i - index)));
+            index = i + 1;
+
+            if(node) q.push(node);
+            if(!head){
+                head = node;
+                continue;
+            } 
+
+            if(left) q.front()->left = node;
+            else q.front()->right = node, q.pop();
+            left = !left;
+        }
+
+    return head;
+}
+
+void print(vector<int> v){
+    cout << "[";
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i];
+        if(i != v.size() - 1) cout << ",";
+    }
+    cout << "]";
+    cout << endl;
+}
+
+class Solution {
+    int max_cnt, cnt, prev;
+    void inorder(vector<int>& ans, TreeNode* node){
+        if(!node) return;
+        inorder(ans, node->left);
+        
+        cnt = prev == node->val ? cnt + 1 : 1;
+        prev = node->val;
+
+        if(cnt > max_cnt) ans.clear(), ans.push_back(prev), max_cnt = cnt;
+        else if(cnt == max_cnt) ans.push_back(prev);
+
+        inorder(ans, node->right);
+    }
+public:
+    vector<int> findMode(TreeNode* root) {
+        vector<int> ans;
+        
+        max_cnt = cnt = 0;
+        prev = -0x80000000;
+
+        inorder(ans, root);
+        return ans;
+    }
+};
+
+int main() {
+    while(true) {
+        Solution solution;
+        string s;
+        cout << "root = ";
+        cin >> s;
+        auto root = parse(s);
+
+        print(solution.findMode(root));
+    }
+    
+    return 0;
+}
