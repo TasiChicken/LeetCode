39bfc728509e8c058415c40499a2aad48e6e047d
TasiChicken <ien940714@gmail.com>
2026-05-04 11:29:35 +0800

    finish 606

---
+++ cpps/1000/700/10/606.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* parse(string input) {
+    TreeNode* head = nullptr;
+    input.erase(input.length() - 1, 1);
+    input.erase(0, 1);
+    
+    if(input.length() == 0) return head;
+
+    int index = 0;
+    bool left = true;
+    queue<TreeNode*> q;
+    for(int i = 0; i <= input.length(); i++) 
+        if(input[i] == ',' || i == input.length()) {
+            auto sub = input.substr(index, i - index);
+
+            TreeNode* node = nullptr;
+            if(sub != "null") node = new TreeNode(stoi(input.substr(index, i - index)));
+            index = i + 1;
+
+            if(node) q.push(node);
+            if(!head){
+                head = node;
+                continue;
+            } 
+
+            if(left) q.front()->left = node;
+            else q.front()->right = node, q.pop();
+            left = !left;
+        }
+
+    return head;
+}
+
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if(!root) return "";
+        string ans = to_string(root->val);
+        if(!root->left && !root->right) return ans;
+
+        ans += "(" + tree2str(root->left) + ")";
+        if(root->right) ans += "(" + tree2str(root->right) + ")";
+        return ans;
+    }
+};
+
+int main() {
+    while(true) {
+        Solution solution;
+        string s;
+        cin >> s;
+        auto root = parse(s);
+
+        cout << solution.tree2str(root) << endl;
+    }
+    
+    return 0;
+}
